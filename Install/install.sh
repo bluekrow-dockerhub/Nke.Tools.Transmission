991319ac5cb25d65b88a3d3656d7486a0ac52676
BlueKrow-DockerHub <ndioses+dockerhub@gmail.com>
2021-01-07 13:52:04 +0100

    refactor: clean up for install script

--- Install/install.sh
+++ Install/install.sh
@@ -1,14 +1,31 @@
 #!/bin/sh
 set -eu
 
-sudo rm -rf /opt/docker-transmission
-sudo mkdir -p /opt/docker-transmission
+SERVICE_NAME="Docker Transmission"
+SERVICE_UNIT="transmission.docker"
+SERVICE_PATH="docker-transmission"
 
-sudo cp transmission-start.sh /opt/docker-transmission/
-sudo cp transmission-stop.sh /opt/docker-transmission/
+echo "REINSTALLING: $SERVICE_NAME"
+echo "---------------------------"
 
-sudo cp transmission.docker.service /etc/systemd/system/
+echo "Disabling old $SERVICE_NAME service"
+sudo systemctl disable ${SERVICE_UNIT}
+echo "Stopping old $SERVICE_NAME service"
+sudo systemctl stop ${SERVICE_UNIT}
 
-sudo systemctl start transmission.docker
-sudo systemctl enable transmission.docker
-sudo systemctl status transmission.docker
+echo "Removing $SERVICE_NAME scripts"
+sudo rm -rf /opt/${SERVICE_PATH}
+echo "Reinstalling new $SERVICE_NAME scripts"
+sudo mkdir -p /opt/${SERVICE_PATH}
+sudo cp ${SERVICE_UNIT}-start.sh /opt/${SERVICE_PATH}/
+sudo cp ${SERVICE_UNIT}-stop.sh /opt/${SERVICE_PATH}/
+echo "Reinstalling new $SERVICE_NAME service unit"
+sudo cp ${SERVICE_UNIT}.service /etc/systemd/system/
+sudo chmod 644 /etc/systemd/system/${SERVICE_UNIT}.service
+
+echo "Starting new $SERVICE_NAME service"
+sudo systemctl start ${SERVICE_UNIT}
+echo "Enabling new $SERVICE_NAME service"
+sudo systemctl enable ${SERVICE_UNIT}
+echo "Resport new $SERVICE_NAME status"
+sudo systemctl status ${SERVICE_UNIT}
