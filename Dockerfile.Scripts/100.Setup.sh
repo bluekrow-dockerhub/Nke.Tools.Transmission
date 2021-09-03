3026d9ef0c079cc6fc9192e96acb6e7c577d3049
BlueKrow-DockerHub <ndioses+dockerhub@gmail.com>
2021-09-03 15:40:06 +0200

    fix: copy settings.json at run not at build

--- Dockerfile.Scripts/100.Setup.sh
+++ Dockerfile.Scripts/100.Setup.sh
@@ -2,5 +2,3 @@
 set -eu
 
 apk add transmission-daemon
-
-cp 110.settings.json /trx/Config/settings.json
--- Dockerfile.Scripts/200.Transmission.sh
+++ Dockerfile.Scripts/200.Transmission.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 set -eu
 
+cp -n Dockerfile.Scripts/110.settings.json /trx/Config/settings.json
+
 transmission-daemon \
  --foreground \
  --config-dir "/trx/Config" \
