6fbb6b1d8cf52fd5ac101f02434d19ac2f9c93d3
BlueKrow-DockerHub <ndioses+dockerhub@gmail.com>
2021-09-15 23:37:35 +0200

    chore: add tests on ci pipeline and remove -t parameter on test runs

--- localhost.tests.sh
+++ localhost.tests.sh
@@ -1,40 +1,49 @@
 #!/bin/sh
+set -eu 
 
 echo
-echo INITIALIZE LOCAL TESTING
+echo INITIALIZE CLOUD TESTING
 echo ------------------------
 TRX_IMAGE="trx-img"
 TRX_CONTAINER="trx-cnt"
 TEST_SCRIPT="dockerhub.tests.sh"
+echo "TRX_IMAGE: $TRX_IMAGE"
+echo "TRX_CONTAINER: $TRX_CONTAINER"
+echo "TEST_SCRIPT: $TEST_SCRIPT"
 
 echo
-echo REMOVING CONTAINERS AND IMAGES
-echo ------------------------------
-docker stop $TRX_CONTAINER
-docker rm $TRX_CONTAINER
-if [ "$1" = "no-cache" ]; then 
-    echo "Enforcing option $1"
-    sudo docker rmi $TRX_IMAGE
-fi 
-
-echo
-echo REBUILD IMAGE FROM DOCKERFILE
-echo -----------------------------
-docker build --tag $TRX_IMAGE .
+echo BUILD IMAGE FROM DOCKERFILE
+echo ---------------------------
+docker build --no-cache --quiet --tag $TRX_IMAGE .
+docker images $TRX_IMAGE
 
 echo
 echo CREATE NEW CONTAINER
 echo --------------------
-#run options --> d:detach i:interactive t:tty
-docker run -d -it --name $TRX_CONTAINER $TRX_IMAGE sh
+docker run -id --name $TRX_CONTAINER $TRX_IMAGE sh
+docker ps 
 
 echo
-echo RUN CLOUD TESTS LOCALLLY
-echo ------------------------
+echo COPY TEST SCRIPTS
+echo -----------------
 docker cp $TEST_SCRIPT $TRX_CONTAINER:$TEST_SCRIPT
-docker exec -it $TRX_CONTAINER sh $TEST_SCRIPT
+echo "$TEST_SCRIPT copied into container"
 
 echo
-echo STOPPING CONTAINER
+echo RUN CLOUD TESTS 
+echo ---------------
+docker exec -i $TRX_CONTAINER sh $TEST_SCRIPT
+
+echo
+echo CONTAINER TEARDOWN
+echo ------------------
+echo Stopping container
 docker stop $TRX_CONTAINER
-echo FINISHED
+echo Removing container
+docker rm $TRX_CONTAINER
+echo Removing image 
+docker rmi $TRX_IMAGE
+
+echo
+echo TESTING FINISHED
+echo ________________
